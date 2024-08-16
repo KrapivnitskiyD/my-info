1d7163bef29064ef460376ae148118c2f2750af3
krapivnitskiyD <krapivnitskiy@itstep.academy>
2024-08-16 16:12:53 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+function AboutMe() {
+    const info = {
+        fullName: 'Крапивницкий Дмитрий Дмитриевич',
+        phone: '0632225551',
+        email: 'krapivnitskiy@itstep.academy'
+    };
+
+    const infoStyle = {
+
+        display: 'inline-block',
+        margin: '20px',
+        padding: '10px',
+        backgroundColor: '#edffb3',
+        borderRadius: '15px',
+        boxShadow: '0px 0px 3px gray',
+        fontSize: '1.4em',
+        fontFamily: 'Courier, monospace'
+    };
+
+    return (
+        <div style={infoStyle}>
+            <h2>{info.fullName}</h2>
+            <p><strong>Тел.: </strong>{info.phone}</p>
+            <p><strong>Эл. почта: </strong>{info.email}</p>
+        </div>
+    );
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AboutMe></AboutMe>
+        </div>
+    );
 }
 
 export default App;
